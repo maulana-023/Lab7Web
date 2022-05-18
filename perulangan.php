4b12aaf400dfd3b6bcec2ce17b25712d70a89c85
Maulana <maulanamuhamad023@gmail.com>
2022-05-18 23:25:15 +0700

    DONE

---
+++ perulangan.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h2>Peluang For</h2>
+    <?php
+echo "Perulangan 1 sampai 10 <br />";
+for ($i=1; $i<=10; $i++) {
+echo "Perulangan ke: " . $i . '<br />';
+}
+echo "Perulangan Menurun dari 10 ke 1 <br />";
+for ($i=10; $i>=1; $i--) {
+echo "Perulangan ke: " . $i . '<br />';
+}
+?>
+    <h2>Peluang While</h2>
+    <?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+while ($i<=10) {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+}
+?>
+    <h2>Peluang Dowhile</h2>
+    <?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+do {
+echo "Perulangan ke: " . $i . '<br />';
+$i++;
+} while ($i<=10);
+?>
+</body>
+</html>
---
+++ php_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+        echo "Hello World";
+    ?>
+    <!-- Variabel PHP -->
+    <h2>Menggunakan Variabel</h2>
+    <?php
+        $nim = "312010188";
+        $nama = 'Maulana Muhamad';
+        echo "NIM : " . $nim . "<br>";
+        echo "Nama : $nama";
+    ?>
